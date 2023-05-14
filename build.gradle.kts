666b8aad742a4c119c5e76ce2ed628fa6365408e
wangxiang <1827945911@qq.com>
2023-05-14 23:49:28 +0800

    perf: init platform config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,6 @@
-plugins {
-	java
-	id("org.springframework.boot") version "3.0.6"
-	id("io.spring.dependency-management") version "1.1.0"
-}
-
+version = "1.0.0"
 group = "com.tianli.auth"
-version = "0.0.1-SNAPSHOT"
+description = "for grasscutter authorization dispatch"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 configurations {
@@ -14,23 +9,46 @@ configurations {
 	}
 }
 
+val hutoolVersion:String by extra { "5.8.18" }
+val mybatisVersion:String by extra { "3.0.0" }
+val lombokVersion:String by extra { "1.18.26" }
+val mysqlVersion:String by extra { "8.0.33" }
+// todo: testings
+val appName:String by extra { rootProject.name }
+
 repositories {
 	mavenCentral()
+	maven {
+		url = uri("https://maven.aliyun.com/repository/public")
+	}
 }
 
 dependencies {
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	implementation("org.springframework.boot:spring-boot-starter-mustache")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
-	implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:3.0.0")
-	compileOnly("org.projectlombok:lombok")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	runtimeOnly("com.mysql:mysql-connector-j")
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
+	implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:$mybatisVersion")
+	implementation("cn.hutool:hutool-core:$hutoolVersion")
+	runtimeOnly("com.mysql:mysql-connector-j:$mysqlVersion")
+	compileOnly("org.projectlombok:lombok:$lombokVersion")
+	annotationProcessor("org.projectlombok:lombok:$lombokVersion")
+}
+
+plugins {
+	java
+	id("org.springframework.boot") version "3.0.6"
+	id("io.spring.dependency-management") version "1.1.0"
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+tasks.processResources {
+	filesMatching("application.yml") {
+		expand(project.properties)
+	}
 }
---
+++ gradle.properties.kts
@@ -0,0 +1,5 @@
+project.url="https://github.com/wander-in-wind/grasscutter-auth"
+org.gradle.daemon=true
+org.gradle.jvmargs="-Xmx2g -Dfile.encoding=UTF-8"
+org.gradle.parallel=true
+org.gradle.configureondemand=true
