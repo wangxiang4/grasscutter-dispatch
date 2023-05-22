6039f7168fcb1ad1e23a5b66782899b171c8b758
wangxiang <1827945911@qq.com>
2023-05-22 12:47:00 +0800

    chore: eliminate

--- src/main/java/com/tianli/auth/grasscutterauth/controller/LoginController.kt
+++ src/main/java/com/tianli/auth/grasscutterauth/controller/LoginController.kt
@@ -2,13 +2,9 @@ package com.tianli.auth.grasscutterauth.controller;
 
 import com.tianli.auth.grasscutterauth.domain.User
 import com.tianli.auth.grasscutterauth.service.LoginService
-import freemarker.template.Configuration
 import lombok.RequiredArgsConstructor
-import org.springframework.mail.javamail.JavaMailSender
-import org.springframework.mail.javamail.MimeMessageHelper
 import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Mono
-import java.io.StringWriter
 
 @RestController
 @RequiredArgsConstructor
@@ -19,5 +15,4 @@ class LoginController(private val loginService:LoginService) {
     fun getUserById(@PathVariable id:Long):Mono<User>  {
         return Mono.just(loginService.getUserById(id))
     }
-
 }
