b3fe5bc15b98c329380d64416f83101e9284dba4
wangxiang <1827945911@qq.com>
2023-06-01 18:34:52 +0800

    chore: update sql

--- docs/sql/grasscutter_dispatch.sql
+++ docs/sql/grasscutter_dispatch.sql
@@ -11,44 +11,40 @@
  Target Server Version : 50739 (5.7.39)
  File Encoding         : 65001
 
- Date: 29/05/2023 22:58:52
+ Date: 01/06/2023 18:34:20
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for sys_user
+-- Table structure for dispatch_user
 -- ----------------------------
-DROP TABLE IF EXISTS `sys_user`;
-CREATE TABLE `sys_user` (
-  `id` varchar(32) NOT NULL,
+DROP TABLE IF EXISTS `dispatch_user`;
+CREATE TABLE `dispatch_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'uid',
+  `create_player_id` int(11) DEFAULT '0' COMMENT '指定首次进入游戏服务器创建的游戏id',
   `user_name` varchar(30) DEFAULT NULL COMMENT '用户账号',
-  `nick_name` varchar(30) DEFAULT NULL COMMENT '用户昵称',
   `password` varchar(100) DEFAULT '' COMMENT '密码',
   `email` varchar(50) DEFAULT '' COMMENT '用户邮箱',
   `phone` varchar(11) DEFAULT '' COMMENT '手机号码',
-  `sex` char(1) DEFAULT '0' COMMENT '用户性别（0男 1女 2未知）',
-  `avatar` varchar(100) DEFAULT '' COMMENT '头像地址',
-  `status` char(1) DEFAULT '0' COMMENT '帐号状态（0正常 1停用）',
-  `login_ip` varchar(50) DEFAULT '' COMMENT '最后登陆IP',
-  `login_time` datetime DEFAULT NULL COMMENT '最后登陆时间',
-  `create_by_id` varchar(32) DEFAULT NULL COMMENT '创建id',
-  `create_by_name` varchar(64) DEFAULT NULL COMMENT '创建者',
+  `token` text COMMENT '游戏服务器验证账户信息token',
+  `session_key` text COMMENT '自动登录会话key',
+  `locale` varchar(80) DEFAULT NULL COMMENT '游戏服务器国际化',
+  `ban_start_time` int(11) DEFAULT '0' COMMENT '账户开始禁用时间',
+  `ban_end_time` int(11) DEFAULT '0' COMMENT '账户结束禁用时间',
+  `ban` char(1) DEFAULT '0' COMMENT '禁用账户',
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
-  `update_by_id` varchar(32) DEFAULT NULL COMMENT '更新id',
-  `update_by_name` varchar(64) DEFAULT '' COMMENT '更新者',
   `update_time` datetime DEFAULT NULL COMMENT '更新时间',
   `remarks` varchar(500) DEFAULT NULL COMMENT '备注',
   `del_flag` char(1) DEFAULT '0' COMMENT '删除标志（0代表存在 1代表删除）',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户信息表';
+) ENGINE=InnoDB AUTO_INCREMENT=10001 DEFAULT CHARSET=utf8mb4 COMMENT='用户信息表';
 
 -- ----------------------------
--- Records of sys_user
+-- Records of dispatch_user
 -- ----------------------------
 BEGIN;
-INSERT INTO `sys_user` (`id`, `user_name`, `nick_name`, `password`, `email`, `phone`, `sex`, `avatar`, `status`, `login_ip`, `login_time`, `create_by_id`, `create_by_name`, `create_time`, `update_by_id`, `update_by_name`, `update_time`, `remarks`, `del_flag`) VALUES ('1510539584287346688', 'admin', '管理员', '$2a$10$jvVGvS6QOtD.x3UwuZRSgepL.zKul6NRg2kVnRVDUdeN2FErIQgNy', '111@qq.com', '15611111111', '0', '', '0', '', NULL, '1510539584287346688', 'admin', '2020-03-13 20:56:44', '1510539584287346688', 'admin', '2022-08-26 15:26:17', '全栈工程师', '0');
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
