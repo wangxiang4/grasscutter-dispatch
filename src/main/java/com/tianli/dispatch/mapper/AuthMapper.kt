31e81f6f84f22c4ea79dc3ae96cd7b31ea13b297
wangxiang <1827945911@qq.com>
2023-06-02 23:40:29 +0800

    chore: mapper

--- src/main/java/com/tianli/dispatch/mapper/AuthMapper.kt
+++ src/main/java/com/tianli/dispatch/mapper/AuthMapper.kt
@@ -5,7 +5,6 @@ import org.apache.ibatis.annotations.Select
 
 interface AuthMapper {
 
-    @Select("SELECT * FROM dispatch_account WHERE token = #{token}")
     fun getUserByToken(token:String): Account
 
     fun register(account: Account): Int
